b373eb0b02cbb35f07fe8d21ff7a7be880c12d5b
“AbdurRaahimm” <rahim703936@gmail.com>
2023-06-27 16:17:48 +0600

    wp react plugin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Charts from './components/Charts';
+
+const App = () => {
+    return (
+        <div>
+            <h1>React Widget</h1>
+            <Charts />
+        </div>
+     );
+}
+
+export default App; 
